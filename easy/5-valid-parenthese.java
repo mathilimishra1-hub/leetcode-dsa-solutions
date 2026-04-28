df2ee7a53aff03c4af1b235f1dee733d2fe2d0ae
mathilimishra1-hub <mathilimishra1@gmail.com>
2026-04-28 17:05:05 +0530

    Create 5-valid-parenthese.java

---
+++ easy/5-valid-parenthese.java
@@ -0,0 +1,29 @@
+link:-https://leetcode.com/problems/valid-parentheses/
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            // If opening bracket, push to stack
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+            // If closing bracket
+            else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') return false;
+                if (ch == '}' && top != '{') return false;
+                if (ch == ']' && top != '[') return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
