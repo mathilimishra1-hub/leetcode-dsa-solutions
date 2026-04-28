e96175e2acfc14921577ef84d255b60cba1076a0
mathilimishra1-hub <mathilimishra1@gmail.com>
2026-04-28 17:08:17 +0530

    Add removeDuplicates method in Solution class
    Implement a method to remove duplicates from a sorted array.

---
+++ easy/7-remove-duplicates-from-sorted.java
@@ -0,0 +1,24 @@
+link:-https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+     
+        if (nums.length == 0) {
+            return 0;
+        }
+
+     
+        int i = 0;
+
+      
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;                // Move unique pointer
+                nums[i] = nums[j];  // Update with new unique value
+            }
+        }
+
+    
+        return i + 1;
+    }
+}
