759e9a63eb9621348c5bd3e02795f4e34e2bfe4f
mathilimishra1-hub <mathilimishra1@gmail.com>
2026-04-29 15:00:13 +0530

    Create 11-Swap-Nodes-in-Pairs.java

---
+++ medium/11-Swap-Nodes-in-Pairs.java
@@ -0,0 +1,27 @@
+link:-https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        // Dummy node to handle edge cases
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        
+        ListNode prev = dummy;
+        
+        while (prev.next != null && prev.next.next != null) {
+            
+            ListNode first = prev.next;
+            ListNode second = first.next;
+            
+            // Swapping
+            first.next = second.next;
+            second.next = first;
+            prev.next = second;
+            
+            // Move prev forward
+            prev = first;
+        }
+        
+        return dummy.next;
+    }
+}
